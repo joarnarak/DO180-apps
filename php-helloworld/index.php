7993b0ad69c5551db4fe0a9be271638b1cd66d27
Student User <student@workstation.lab.example.com>
2021-02-10 04:05:49 -0500

    nuevo cambio

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
+  print "This is version 1 of the app. I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
 ?>
